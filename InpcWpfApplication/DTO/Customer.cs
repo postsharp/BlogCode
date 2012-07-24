0b4690be0dccf14db1f1ea95609e69ffcb282cdd
gfraiteur <gael@sharpcrafters.com>
2012-07-24 11:33:49 +0200

    Sample application for INotifyPropertyChanged.

---
+++ InpcWpfApplication/DTO/Customer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PostSharp.Toolkit.Domain;
+
+namespace InpcWpfApplication.DTO
+{
+    [NotifyPropertyChanged]
+    class Entity
+    {
+
+    }
+
+
+    class Customer : Entity
+    {
+        private string _firstName;
+        private string _lastName;
+        private Address _address;
+        
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public Address Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public string FullName { get { return this.GetFullName(); } }
+
+        private string GetFullName()
+        {
+            return string.Join(" ", this._firstName,this.LastName);
+        }
+
+        public void Reset()
+        {
+            this._firstName = null;
+            this._lastName = null;
+            this._address = null;
+        }
+
+
+    }
+
+    class Address : Entity
+    {
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Town { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
---
+++ InpcWpfApplication/ModelView/CustomerModelView.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InpcWpfApplication.DTO;
+using PostSharp.Toolkit.Domain;
+
+namespace InpcWpfApplication.ModelView
+{
+    [NotifyPropertyChanged]
+    class CustomerModelView
+    {
+        private Customer customer;
+
+        public CustomerModelView(Customer customer)
+        {
+            this.customer = customer;
+        }
+
+        [DependsOn("customer.FirstName")]
+        public string FirstName
+        {
+            get { return this.customer.FirstName; }
+            set { this.customer.FirstName = value; }
+        }
+
+        [DependsOn("customer.LastName")]
+        public string LastName
+        {
+            get { return this.customer.LastName; }
+            set { this.customer.LastName = value; } 
+        }
+
+        [DependsOn("customer.Address")]
+        public Address Address { get { return this.customer.Address; } }
+
+        [DependsOn("customer.FullName")]
+        public string FullName { get { return this.customer.FullName; } }
+
+         [DependsOn("FirstName", "LastName", "customer.Address.Line1", "customer.Address.Line2", "customer.Address.PostalCode", "customer.Address.Town")]
+        public string BusinessCard
+        {
+            get
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.Append(this.FirstName);
+                stringBuilder.Append(" ");
+                stringBuilder.Append(this.LastName);
+                stringBuilder.AppendLine();
+                stringBuilder.AppendLine(this.customer.Address.Line1);
+                string line2 = this.customer.Address.Line2;
+                if (!string.IsNullOrWhiteSpace(line2))
+                {
+                    stringBuilder.AppendLine(line2);
+                }
+                stringBuilder.Append(this.customer.Address.PostalCode);
+                stringBuilder.Append(' ');
+                stringBuilder.Append(this.customer.Address.Town);
+
+                return stringBuilder.ToString();
+            }
+        }
+
+
+       
+    }
+}
